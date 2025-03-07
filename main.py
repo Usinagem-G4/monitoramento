eea5504f75c94a6368d57accbcdf2e6d5a22dad1
Usinagem-G4 <usinagem@g4maquinas.com.br>
2025-03-07 11:47:04 -0300

    Update main.py

--- main.py
+++ main.py
@@ -135,7 +135,7 @@ def main():
             st.dataframe(
                 df.style.applymap(
                     lambda x: 'background-color: #ff0000; color: white' if x == 'Expirado' else '',
-                    subset=(['Tempo restante'], ['Operador'])
+                    subset= ['Tempo restante'], ['Operador']
                 ),
                 height=600
             )
